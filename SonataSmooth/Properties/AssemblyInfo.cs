e608f7879883911881156c97bf29d52bccd7db58
Charlie Jaewoong Mun <happybono@outlook.com>
2025-08-15 18:07:56 +0900

    Add files via upload

--- SonataSmooth/Properties/AssemblyInfo.cs
+++ SonataSmooth/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.2.6.1")]
-[assembly: AssemblyFileVersion("4.2.6.1")]
+[assembly: AssemblyVersion("4.2.7.0")]
+[assembly: AssemblyFileVersion("4.2.7.0")]
