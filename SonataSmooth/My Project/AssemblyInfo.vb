50967d10948991da60af919c03d04539fe040596
Charlie Jaewoong Mun <happybono@outlook.com>
2025-08-20 17:08:46 +0900

    Add files via upload

---
+++ SonataSmooth/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("AvocadoSmoothie")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Codies")>
+<Assembly: AssemblyProduct("AvocadoSmoothie")>
+<Assembly: AssemblyCopyright("Copyright © HappyBono 2025")>
+<Assembly: AssemblyTrademark("AvocadoSmoothie")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2d09c302-eb82-4964-80fb-1e853188617b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("4.3.0.0")>
+<Assembly: AssemblyFileVersion("4.3.0.0")>
